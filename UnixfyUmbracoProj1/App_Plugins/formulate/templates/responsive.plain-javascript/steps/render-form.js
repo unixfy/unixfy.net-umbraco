3d35cbd91e3be4fb56aac2b5d1a9a58717f523b6
Alex Wang <admin@unixfy.me>
2020-05-10 17:34:12 -0400

    Initial upload from VS

---
+++ UnixfyUmbracoProj1/App_Plugins/formulate/templates/responsive.plain-javascript/steps/render-form.js
@@ -0,0 +1,76 @@
+/**
+ * Renders a Formulate form.
+ * @param formData The data to render a form for.
+ * @param formElement The DOM element to insert elements into.
+ * @param placeholderElement The element that the form will be inserted before.
+ * @param fieldRenderers The associative array of field rendering functions.
+ * @param fieldValidators The associative array of the field validating functions.
+ */
+function renderForm(formData, formElement, placeholderElement, fieldRenderers, fieldValidators) {
+
+    // Variables.
+    let i, j, k, row, rows, rowElement, cells, cell, fields, fieldId,
+        columnCount, cellElement, fieldElement, fieldsData, fieldMap,
+        field, renderedFields, renderedField, stepIndex, isActiveStep;
+
+    // Map fields to an associative array for quick lookups.
+    fieldsData = formData.data.fields;
+    fieldMap = require("../utils/map-fields-by-id")(fieldsData);
+
+    // Process each row in the form.
+    rows = formData.data.rows;
+    renderedFields = [];
+    isActiveStep = true;
+    stepIndex = 0;
+    for (i = 0; i < rows.length; i++) {
+
+        // Create the row.
+        row = rows[i];
+        cells = row.cells;
+        if (row.isStep) {
+            stepIndex++;
+            isActiveStep = false;
+            continue;
+        } else {
+            rowElement = require("./render-row")(stepIndex, isActiveStep);
+            formElement.appendChild(rowElement);
+        }
+
+        // Process each cell in this row.
+        for (j = 0; j < cells.length; j++) {
+
+            // Create the cell.
+            cell = cells[j];
+            fields = cell.fields;
+            columnCount = cell.columns;
+            cellElement = require("./render-cell")(columnCount);
+            rowElement.appendChild(cellElement);
+
+            // Process each field in this cell.
+            for (k = 0; k < fields.length; k++) {
+
+                // Create the field.
+                fieldId = fields[k].id;
+                field = fieldMap[fieldId];
+                renderedField = require("./render-field")(field, fieldRenderers, fieldValidators, {
+                    formElement: formElement,
+                    placeholderElement: placeholderElement,
+                    stepIndex
+                });
+                renderedFields.push(renderedField);
+                fieldElement = renderedField.getElement();
+                cellElement.appendChild(fieldElement);
+
+            }
+
+        }
+
+    }
+
+    // Return the rendered fields.
+    return renderedFields;
+
+}
+
+// Export the function that renders a form.
+module.exports = renderForm;
